01c1795221dbe8973539cfb740f2816152a21ee1
Clement <clement.beal@outlook.com>
2024-11-23 14:06:32 -0300

    can write the header and the song title

--- lib/src/writers/id3v4_writer.dart
+++ lib/src/writers/id3v4_writer.dart
@@ -1,21 +1,69 @@
+import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:audio_metadata_reader/src/metadata/mp3_metadata.dart';
-import 'package:audio_metadata_reader/src/utils/bit_manipulator.dart';
+
+class TagHeader {
+  final int majorVersion;
+  final int minorVersion;
+  final int size;
+  final bool hasFooter;
+  final bool hasExtendedHeader;
+
+  TagHeader(this.majorVersion, this.minorVersion, this.size, this.hasFooter,
+      this.hasExtendedHeader);
+
+  int get version => majorVersion * 100 + minorVersion;
+}
 
 class Id3v4Writer {
   void write(File file, Mp3Metadata metadata) {
-    // final writer = file.();
+    // check if the file has an ID3 metadata
+    final size = file.lengthSync();
 
     final builder = BytesBuilder();
 
-    _writeHeader(builder);
+    if (metadata.songName != null) {
+      _writeFrame(builder, "TIT2", metadata.songName!);
+    }
 
-    file.writeAsBytesSync(builder.toBytes());
+    final finalBuilder = BytesBuilder();
+    _writeHeader(finalBuilder, builder.length);
+    finalBuilder.add(builder.toBytes());
+
+    if (size == 0) {
+      file.writeAsBytesSync(finalBuilder.toBytes());
+    } else {
+      final oldData = file.readAsBytesSync();
+      file.writeAsBytesSync([
+        ...finalBuilder.toBytes(),
+        ...oldData,
+      ]);
+    }
   }
 
-  void _writeHeader(BytesBuilder builder) {
+  void _writeFrame(BytesBuilder builder, String frameId, String data) {
+    builder.add(frameId.codeUnits);
+
+    builder.add(_encodeSynchsafeInteger(data.length + 1));
+    // flags
+    builder.add([0, 0]);
+
+    builder.addByte(0x03);
+    builder.add(utf8.encode(data));
+  }
+
+  Uint8List _encodeSynchsafeInteger(int value) {
+    return Uint8List.fromList([
+      (value >> 21) & 0x7F,
+      (value >> 14) & 0x7F,
+      (value >> 7) & 0x7F,
+      value & 0x7F,
+    ]);
+  }
+
+  void _writeHeader(BytesBuilder builder, int dataSize) {
     // ID3
     builder.addByte(0x49);
     builder.addByte(0x44);
@@ -30,6 +78,6 @@ class Id3v4Writer {
     builder.addByte(0);
 
     // write ID3 metadata size
-    builder.add(intToUint32(0));
+    builder.add(_encodeSynchsafeInteger(dataSize));
   }
 }
--- test/writers/id3v4_writer_test.dart
+++ test/writers/id3v4_writer_test.dart
@@ -2,6 +2,8 @@ import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:audio_metadata_reader/src/metadata/mp3_metadata.dart';
+import 'package:audio_metadata_reader/src/parser.dart';
+import 'package:audio_metadata_reader/src/parsers/id3v2.dart';
 import 'package:audio_metadata_reader/src/writers/id3v4_writer.dart';
 import 'package:test/test.dart';
 
@@ -29,6 +31,45 @@ void main() {
         expect(readResult.getUint8(5), equals(0));
         expect(readResult.getUint32(6), equals(0));
       });
+      test("Empty ID3 header but with a MP3 frame header", () {
+        final writer = Id3v4Writer();
+        final frameData = mp3FrameHeaderCBR();
+        final file = createTemporaryFile("test.mp3", frameData);
+
+        writer.write(file, Mp3Metadata());
+
+        final fileData = file.readAsBytesSync();
+        final readResult = ByteData.sublistView(fileData.sublist(0, 10));
+
+        expect(readResult.getUint8(0), equals(0x49));
+        expect(readResult.getUint8(1), equals(0x44));
+        expect(readResult.getUint8(2), equals(0x33));
+
+        expect(readResult.getUint8(3), equals(4));
+        expect(readResult.getUint8(4), equals(0));
+        expect(readResult.getUint8(5), equals(0));
+        expect(readResult.getUint32(6), equals(0));
+        expect(fileData.length, 10 + frameData.length);
+      });
+
+      test(
+        "Write basic metadata",
+        () {
+          final writer = Id3v4Writer();
+
+          final file = createTemporaryFile("test.mp3", mp3FrameHeaderCBR());
+
+          final metadata = Mp3Metadata();
+          metadata.songName = "Only Ones Who Know";
+
+          writer.write(file, metadata);
+
+          final resultMetadata =
+              ID3v2Parser().parse(file.openSync()) as Mp3Metadata;
+
+          expect(resultMetadata.songName, equals(metadata.songName));
+        },
+      );
     },
   );
 }

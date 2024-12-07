4a43457026d80d1e24a5a538f1f4026c6bf32fbe
ClementBeal <49765303+ClementBeal@users.noreply.github.com>
2024-12-07 11:58:28 -0300

    feat: add Flac metadata writer (#44)
    * fix: ID3v2 lyrics with UTF-16 (#27)
    
    * fix: read all metadata from Opus (#28)
    
    * fix: id3v1 metadata with ASCII (#30)
    
    * feat: use different exception if there's no available parser (#29)
    
    * fix: sometimes the track value is empty (#31)
    
    * Opus speed up + doc (#32)
    
    * an OGG a bit faster
    
    * speed up the Opus parsing
    
    * skip the content when we only want the header
    
    * do not fetch the OGG image if not needed
    
    * doc
    
    * doc
    
    * doc
    
    * fix: ogg perf and mp4 parsing (#34)
    
    * an OGG a bit faster
    
    * speed up the Opus parsing
    
    * skip the content when we only want the header
    
    * do not fetch the OGG image if not needed
    
    * doc
    
    * doc
    
    * doc
    
    * fix: OGG is parsed correctly
    
    * don't use dynamic
    
    * rename the TextFrame function
    
    * use else if
    
    * fix: reset position for MP4
    
    * fix bad changelod
    
    * fix: update changelog
    
    * fix: exit if MP4 is malformed (#35)
    
    * fix: reset the position to 0
    
    * make it a bit faster
    
    * only fetch image for mp4 if needed
    
    * cap
    
    * fix: id3v2 lyric (#36)
    
    * fix: read correctly the lyric
    
    * changelog
    
    * in case it's utf16
    
    * chore: add CI and release actions (#23)
    
    * chore: test and version (#37)
    
    * chore(main): release 1.0.0 (#38)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * feat: support album artist (#39)
    
    * add some doc
    
    * remove useless merge function
    
    * add the performers metadata
    
    * fix: TXXX was wrong
    
    * id3, flac, ogg,opus find performers
    
    * make it work
    
    * fix: ID3v2 wasn't parsing description as UTF-16 (#42)
    
    * fix(id3v2): image's description can be UTF-16
    
    * fix: decription must increase offset
    
    * fix: parse correctly the null character (#43)
    
    * fix: parse correctly the null character
    
    Some ID3v2 have an \0 null character as a year. `int.parse()`throws an exception if it can't translate the string into an integer.
    
    * use tryParse just in case
    
    * feat: write ID3v4 (#25)
    
    * can write emoty tag
    
    * can write the header and the song title
    
    * write all the T frames
    
    * can write pictures
    
    * add the readAllMetadata
    
    * can write
    
    * feat: add ID3v1 parser (#20)
    
    * can rewrite Vorbis comment
    
    * remove padding blocks and copy Flac frames
    
    * can rewrite the image
    
    * fix length
    
    ---------
    
    Co-authored-by: Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- lib/audio_metadata_reader.dart
+++ lib/audio_metadata_reader.dart
@@ -15,3 +15,13 @@ export 'src/writers/id3v4_writer.dart' show Id3v4Writer;
 export 'src/utils/metadata_parser_exception.dart'
     show MetadataParserException, NoMetadataParserException;
 export 'src/parser.dart' show readMetadata;
+export 'src/utils/metadata_parser_exception.dart' show MetadataParserException;
+export 'src/parser.dart' show readMetadata, readAllMetadata;
+
+export 'src/parsers/id3v1.dart' show ID3v1Parser;
+export 'src/parsers/id3v2.dart' show ID3v2Parser;
+export 'src/parsers/flac.dart' show FlacParser;
+export 'src/parsers/mp4.dart' show MP4Parser;
+export 'src/parsers/ogg.dart' show OGGParser;
+
+export 'src/writers/id3v4_writer.dart' show Id3v4Writer;

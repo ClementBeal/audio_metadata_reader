9e812e7dc3cba16875cc72bd44c8ddb3188a09e9
lyp <liu_yupei@foxmail.com>
2026-02-06 01:29:39 +0800

    fix

--- example/main.dart
+++ example/main.dart
@@ -12,18 +12,9 @@ final client = webdav.newClient(
 );
 
 void main() async {
-  final dirs = await client.readDir('/public/Music');
-  int i = 0 ;
-  int j = 0 ;
-  dirs.forEach((v) {
-    readMetadataWebDav(client, v.path!, getImage: true).then((res) {
-        // print(res);
-        i++;
-        print(i);
-      }).catchError((v){
-        print(v);
-      });
-      j++;
-      print('j: $j');
-  });
+  File file = File('/Users/lyp/Music/LZF-Music/After Hours - The Weeknd.flac');
+  Uint8List data = file.readAsBytesSync();
+  print(data.length);
+  final metadata = await readMetadataUint8List(data, getImage: true);
+  print(metadata);
 }
